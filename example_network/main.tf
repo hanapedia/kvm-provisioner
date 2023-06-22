2700815306e2a79b73b7341ed93a7cb85a53a690
Hiroki <hiroki11hanada@gmail.com>
2023-06-22 18:27:58 +0900

    use default pool

--- example_network/main.tf
+++ example_network/main.tf
@@ -4,16 +4,16 @@ resource "libvirt_network" "vlan_network" {
   mode      = "bridge"
   bridge    = "br0"
 }
-#
-# resource "libvirt_pool" "kvm_pool" {
-#   name = "example"
-#   type = "dir"
-#   path = pathexpand("~/.kvm/example")
-# }
-#
+
+resource "libvirt_pool" "default" {
+  name = "default"
+  type = "dir"
+  path = "/var/lib/libvirt/images"
+}
+
 resource "libvirt_volume" "base_image" {
   name   = "base_image"
-  # pool   = "example"
+  pool   = "default"
   source = "https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.img"
   format = "qcow2"
 }
